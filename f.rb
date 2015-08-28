0f6788e6329fd50fa75ac5c99ed8c4717031ef49
Josh Cheek <josh.cheek@gmail.com>
2015-08-28 00:10:53 -0600

    Single-colour starburst

--- f.rb
+++ f.rb
@@ -3,8 +3,22 @@
 require 'io/console'
 $stdin.raw do
 
-  def print_at(x, y, char)
-    print "\e[#{y};#{x}H#{char}"
+  def click_at(x, y)
+    # current window size
+    max_y, max_x = $stdout.winsize
+
+    # each pixel is 2 chars
+    x -= 1 if x.even?
+    max_x /= 2
+
+    burst = [
+      "\e[#{y};1H",                                         # far left
+      "\e[48;5;#{16 + rand(216)}m",                         # random colour
+      "  " * max_x,                                         # horizontal beam
+      *1.upto(max_y).map { |y_off| "\e[#{y_off};#{x}H  " }, # vertical beam
+      "\e[0m",                                              # color off
+    ].join
+    print burst
   end
 
   def get_int
@@ -25,17 +39,16 @@ $stdin.raw do
     print "\e[?1000l" # don't record mouse
   end
 
+  print "How many times would you like to record the mouse? "
+  num_times = get_int
+
   print "\e[?25l"   # hide cursor
   print "\e[H\e[2J" # clear screen (go to top-left and clear to bottom right)
   print "\e[?1000h" # record mouse
 
-  print "How many times would you like to record the mouse? "
-  num_times = get_int
-
   x = y = nil
-  print
   num_times.times do
-    # only works with mouse clicks b/c Im just experimenting, so dont press keys
+    # only works with mouse clicks b/c Im just experimenting... so dont press keys
 
     # mouse down: "\e[M xy"
     4.times { $stdin.getc }
@@ -44,7 +57,7 @@ $stdin.raw do
     x = $stdin.getc.force_encoding(Encoding::ASCII_8BIT).ord - 32
     y = $stdin.getc.force_encoding(Encoding::ASCII_8BIT).ord - 32
 
-    print_at x, y, "\e[4#{rand 8}m  " # print a randomly coloured x there
+    click_at x, y
     6.times { $stdin.getc }           # mouse up: "\e[M#xy"
   end
 end
